884c0728053df61a3db6e15a1e9bab4aaa60bfd9
Aleksander <licklub@wielkopolskizpn.pl>
2021-05-29 17:09:26 +0100

    fixed styled components

--- pages/gry/[gra].js
+++ pages/gry/[gra].js
@@ -7,6 +7,82 @@ import { useRouter } from "next/router";
 import { PeopleFill } from "@styled-icons/bootstrap/PeopleFill";
 import { TimeFive } from "@styled-icons/boxicons-solid/TimeFive";
 import { Child } from "@styled-icons/fa-solid/Child";
+
+const Opis = styled.p`
+  max-width: 800px;
+  width: 100%;
+  margin-bottom: 32px;
+  border-right: 3px solid #daeaf6;
+  padding-right: 48px;
+
+  @media (max-width: 700px) {
+    border-right: 0px;
+    padding-right: 0;
+    border-bottom: 3px solid #daeaf6;
+    padding-bottom: 32px;
+    margin-bottom: 12px;
+  }
+`;
+
+const Zdjecie = styled.img`
+  max-width: 500px;
+  width: 100%;
+`;
+
+const Czas = styled(TimeFive)`
+  width: 25px;
+  color: #ff008e;
+  margin-right: 12px;
+`;
+
+const Ludzie = styled(PeopleFill)`
+  color: #ff008e;
+  width: 25px;
+  margin-right: 12px;
+`;
+
+const Wiek = styled(Child)`
+  color: #ff008e;
+  width: 25px;
+  margin-right: 12px;
+`;
+
+const Rzad = styled.div`
+  display: flex;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
+`;
+
+const Informacje = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 32px;
+
+  & p {
+    display: flex;
+    align-items: center;
+  }
+
+  @media (max-width: 700px) {
+    flex-direction: row;
+    margin-left: 0;
+    justify-content: space-around;
+  }
+`;
+
+const KupTeraz = styled.a`
+  font-weight: bold;
+  padding: 8px 24px;
+  border-radius: 15px;
+  background: #daeaf6;
+  transition: all 0.2s;
+
+  &:hover {
+    background: #ff008e;
+  }
+`;
 const Gra = () => {
   const router = useRouter();
   const { gra } = router.query;
@@ -20,82 +96,6 @@ const Gra = () => {
       return false;
     }
   })[0];
-
-  const Opis = styled.p`
-    max-width: 800px;
-    width: 100%;
-    margin-bottom: 32px;
-    border-right: 3px solid #daeaf6;
-    padding-right: 48px;
-
-    @media (max-width: 700px) {
-      border-right: 0px;
-      padding-right: 0;
-      border-bottom: 3px solid #daeaf6;
-      padding-bottom: 32px;
-      margin-bottom: 12px;
-    }
-  `;
-
-  const Zdjecie = styled.img`
-    max-width: 500px;
-    width: 100%;
-  `;
-
-  const Czas = styled(TimeFive)`
-    width: 25px;
-    color: #ff008e;
-    margin-right: 12px;
-  `;
-
-  const Ludzie = styled(PeopleFill)`
-    color: #ff008e;
-    width: 25px;
-    margin-right: 12px;
-  `;
-
-  const Wiek = styled(Child)`
-    color: #ff008e;
-    width: 25px;
-    margin-right: 12px;
-  `;
-
-  const Rzad = styled.div`
-    display: flex;
-
-    @media (max-width: 700px) {
-      flex-direction: column;
-    }
-  `;
-
-  const Informacje = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-left: 32px;
-
-    & p {
-      display: flex;
-      align-items: center;
-    }
-
-    @media (max-width: 700px) {
-      flex-direction: row;
-      margin-left: 0;
-      justify-content: space-around;
-    }
-  `;
-
-  const KupTeraz = styled.a`
-    font-weight: bold;
-    padding: 8px 24px;
-    border-radius: 15px;
-    background: #daeaf6;
-    transition: all 0.2s;
-
-    &:hover {
-      background: #ff008e;
-    }
-  `;
 
   return (
     <Szkielet>
